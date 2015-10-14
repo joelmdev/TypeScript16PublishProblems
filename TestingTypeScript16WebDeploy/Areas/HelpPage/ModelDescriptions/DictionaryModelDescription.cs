de4b54e420d8f4bb504e5997a0c07c20230c09f1
Joel Marshall <joel@tusksoft.com>
2015-10-14 09:54:58 -0400

    initial commit

---
+++ TestingTypeScript16WebDeploy/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TestingTypeScript16WebDeploy.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ TestingTypeScript16WebDeploy/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace TestingTypeScript16WebDeploy.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
